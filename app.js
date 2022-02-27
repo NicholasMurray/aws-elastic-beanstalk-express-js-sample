4dd7016334cde4220dad16a9e2ea6cb1a2c5370e
Nicholas Murray <murray.nicholas@gmail.com>
2022-02-27 23:48:43 +0000

    Adding random-quote api url

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const fs = require('fs');
 const port = 8080;
+
+let quotes;
+fs.readFile('data/programming-quotes.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  quotes = JSON.parse(data);
+  console.log(quotes[Math.floor(Math.random() * quotes.length)].en);
+});
+
+const getRandomQuote = () => {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  return randomQuote;
+};
+
+app.get('/api/random-quote', (req, res) => {
+  res.send(getRandomQuote());
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
