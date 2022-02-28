cfe811b9da18fa1befa0a1dad2be34f3f9298647
Nicholas Murray <murray.nicholas@gmail.com>
2022-02-28 00:14:55 +0000

    Testing redirect

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const fs = require('fs');
 const port = 8080;
 
+const FRONTEND_URI = 'http://localhost:8081';
+
 let quotes;
 fs.readFile('data/programming-quotes.json', 'utf8', function (err, data) {
   if (err) throw err;
@@ -20,7 +22,15 @@ app.get('/api/random-quote', (req, res) => {
   res.send(getRandomQuote());
 });
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/Hello', (req, res) => {
+  res.send('<h1>Hello World</h1>');
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  res.redirect(`${FRONTEND_URI}`);
+});
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
